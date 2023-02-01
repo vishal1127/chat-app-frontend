c3c0b3aaed5613b702b9342f80007276fdbbb4a1
vishal2796 <vishal.pb.27@gmail.com>
2023-02-01 13:15:34 +0530

    integrated sign in api

--- scripts/auth.js
+++ scripts/auth.js
@@ -3,9 +3,11 @@ const emailInput = document.getElementById("email");
 const phoneInput = document.getElementById("phone");
 const passInput = document.getElementById("password");
 const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
 const alertMsgDiv = document.getElementById("alert-msg");
 
 registerBtn?.addEventListener("click", registerUser);
+loginBtn?.addEventListener("click", loginUser);
 
 async function registerUser(e) {
   e.preventDefault();
@@ -32,6 +34,35 @@ async function registerUser(e) {
         userData
       );
       alertMsg = response.data.message;
+    } catch (error) {
+      console.log("Error:", error);
+      alertMsg = error.response.data.message;
+      alertMsgDiv.style.backgroundColor = "#ac1724";
+    }
+    alertMsgDiv.innerText = alertMsg;
+    alertMsgDiv.style.display = "block";
+    setTimeout(() => {
+      alertMsgDiv.style.display = "none";
+    }, 4000);
+  }
+}
+async function loginUser(e) {
+  e.preventDefault();
+  emailInput.reportValidity();
+  passInput.reportValidity();
+  if (emailInput.checkValidity() && passInput.checkValidity()) {
+    try {
+      const userData = {
+        email: emailInput.value,
+        password: passInput.value,
+      };
+      const response = await axios.post(
+        "http://localhost:3000/loginUser",
+        userData
+      );
+      alertMsg = response.data.message;
+      localStorage.setItem("Authorization", response.data.token);
+      location.href = "./pages/homepage.html";
     } catch (error) {
       console.log("Error:", error);
       alertMsg = error.response.data.message;
