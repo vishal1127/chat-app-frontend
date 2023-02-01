7b28d6e0d1c89e46d337bbe7a1a917d0e5040fc5
vishal2796 <vishal.pb.27@gmail.com>
2023-02-01 17:35:21 +0530

    integrated get chats api

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ window.addEventListener("DOMContentLoaded", loadData);
 const userName = document.querySelector(".UserName");
 const messageInput = document.getElementById("message");
 const sendBtn = document.getElementById("send-btn");
+const chatListData = document.getElementById("chat-list-data");
+const signOutBtn = document.getElementById("signout-btn");
 
 sendBtn.addEventListener("click", sendMessage);
+signOutBtn.addEventListener("click", signOutUser);
 const localUserData = localStorage.getItem("User Details");
 const token = localStorage.getItem("Authorization");
-console.log("token", token);
 
 async function loadData() {
-  console.log("ushdeuhueh", localUserData);
+  try {
+    const allChats = await axios.get("http://localhost:3000/getAllChats", {
+      headers: {
+        authorization: token,
+      },
+    });
+    chatListData.innerHTML = "";
+    listChats(allChats.data.chats);
+  } catch (error) {
+    console.log("Error", error.response.data.message);
+  }
 }
 
 async function sendMessage() {
@@ -29,9 +41,23 @@ async function sendMessage() {
       if (response) {
         messageInput.value = "";
       }
+      loadData();
       console.log("reponse message", response);
     } catch (error) {
-      console.log("Error:", error);
+      console.log("Error:", error.response.data.message);
     }
   }
+}
+
+async function listChats(chats) {
+  chats.forEach((chat) => {
+    chatListData.innerHTML += `<li class="list-group-item"><b>${chat.from}:</b> ${chat.text}</li>`;
+  });
+}
+
+function signOutUser() {
+  console.log("called signout");
+  localStorage.clear();
+  location.href = "http://127.0.0.1:5500/chat-app-frontend/index.html";
+  token = "";
 }
